8a524c416ce6c873a381cf071c73db4c0c03233b
LucasBW-06 <lucas8bitbw@gmail.com>
2026-08-25 10:25:14 -0300

    inicializacao

---
+++ src/main/java/com/capricha/capricha_api/CaprichaApiApplication.java
@@ -0,0 +1,13 @@
+package com.capricha.capricha_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaprichaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaprichaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capricha/capricha_api/CaprichaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capricha.capricha_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaprichaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
